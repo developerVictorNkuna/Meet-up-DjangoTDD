675665eb972a305d5b1e374133ecc459814f2b46
developerVictorNkuna <victor.nkuna@hotmail.com>
2021-08-05 14:53:38 +0200

    Modified my test_models because offoregin key relation Intergrity Error

--- MeetUps/tests/test_views.py
+++ MeetUps/tests/test_views.py
@@ -1,6 +1,17 @@
-from django.test import TestCase
-from MeetUps.models import*
-from django.shortcuts  import reverse
+# from django.test import TestCase ,LiveServerTestCase,Client
+
+# from MeetUps.models import*
+# from django.shortcuts  import reverse
+# from .forms import RegistrationForm
+
+# class MeetUpViewTest(TestCase):
+
+#     @classmethod
+#     def setupTestDat(cls):
+#         #create or get all meetups
+        
+
+        
 
 
-pass
+#     def test_index(request,meetup_slug):
